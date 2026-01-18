4775f2347f5b3178da18cd00dc31064239474f48
Himanshu <tonu8707@gmail.com>
2026-01-18 22:53:00 +0530

    adding emailservice

---
+++ server/HireFlowEmailService/src/main/java/com/hellFire/HireFlowEmailService/HireFlowEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hellFire.HireFlowEmailService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireFlowEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireFlowEmailServiceApplication.class, args);
+	}
+
+}
---
+++ server/HireFlowEmailService/src/test/java/com/hellFire/HireFlowEmailService/HireFlowEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellFire.HireFlowEmailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireFlowEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
